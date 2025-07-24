831d13747fbb54ffe4281da5b264dd36c6ea1966
An <162073486+AnDayno@users.noreply.github.com>
2025-07-25 02:42:12 +0800

    SB: Disable multiple play

--- Assets/Script/BodyDetection/CrashDetector.cs
+++ Assets/Script/BodyDetection/CrashDetector.cs
@@ -8,14 +8,20 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float reloadDelay = 1.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Obstacle") || collision.collider.CompareTag("Snow"))
         {
-            FindObjectOfType<PlayerController>().disableControls();
             crashEffect.Play();
-            GetComponent<AudioSource>().PlayOneShot(crashSFX, 0.5f);
-            Invoke(nameof(ReloadScene), reloadDelay);
+
+            if (!hasCrashed)
+            {
+                hasCrashed = true;
+                GetComponent<AudioSource>().PlayOneShot(crashSFX, 0.5f);
+                FindObjectOfType<PlayerController>().disableControls();
+                Invoke(nameof(ReloadScene), reloadDelay);
+            }
         }
     }
 
